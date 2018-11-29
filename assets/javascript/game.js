ee0b6bddabf18922f0585e8366c301868e6fc3f6
ENK Bond <43937176+ENKBond@users.noreply.github.com>
2018-11-28 22:38:43 -0700

    JS complete, css updates needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,53 @@
-var counter = 0;
-var targetNumber = 55;
-var numberOptions = [10, 11, 12];
-var increment = numberOptions[Math.round(Math.random())];
+$(document).ready(function() {
+
+var userTotal = 0;
+var wins = 0;
+var loss = 0;
+
+// var numberOptions = Math.floor(Math.random()* 12) + 1;
+var targetNumber = Math.floor(Math.random()*120) + 19;
+
+$("#wins").text("Wins: " + wins);
+$("#losses").text("Losses: " + loss);
 
 $("#randomNumber").text(targetNumber);
+console.log(targetNumber);
+
+function restartGame() {
+    targetNumber = Math.floor(Math.random() * 120) + 19;
+    $("#randomNumber").text(targetNumber);
+    userTotal = 0;
+    $("#totalScore").text(userTotal);
+
+}
+
+function winning() {
+    alert("We've got a champion!");
+    wins++;
+    $("#wins").text("Wins: " + wins);
+    restartGame();
+}
+
+function losing() {
+    alert("Sucks to be a loser. Try again!");
+    loss++;
+    $("#losses").text("Losses: " + loss);
+    restartGame();
+}
 
 $(".crystal-image").on("click", function() {
-    counter += increment;
-    alert("your score is: " + counter);
+    var numberOptions = Math.floor(Math.random()* 12) + 1;
+    userTotal = userTotal + numberOptions;
+    $("#totalScore").text(userTotal);
+        if (userTotal == targetNumber) {
+            winning();
+        }
+        else if (userTotal > targetNumber) {
+            losing();
+        }
+    });
+   
 
-    if (counter === targetNumber) {
-        alert("You win!");
-    }
-    else if (counter >= targetNumber) {
-        alert("You Lose");
-    }
-    console.log(counter);
+   
 });
 
